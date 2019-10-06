7fc3cd08909c89fda17e8eae09e15ec53d30eb87
ayogatot <azerino25@gmail.com>
2019-10-06 11:48:26 +0700

    Get Screenshot functions

--- app.js
+++ app.js
@@ -4,4 +4,17 @@ const app = express();
 
 const PORT = 1234;
 
+const getScreenshot = async websiteName => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const imageName = websiteName.split(/[https://]/gi)[8] + ".png";
+  console.log(imageName);
+  await page.goto(websiteName);
+  await page.screenshot({ path: `./ss_image/${imageName}` });
+
+  await browser.close();
+};
+
+getScreenshot("https://komiku.co/");
+
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
